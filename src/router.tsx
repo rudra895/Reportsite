bd50d4db99586ff6a79bfb10f2d4dc4a70c188b5
Rudra Bedawala <rudrabedawala895@gmail.com>
2026-05-16 18:28:19 +0530

    Fix GitHub Pages deployment

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,15 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
+const basepath = import.meta.env.BASE_URL === "/" ? "/" : import.meta.env.BASE_URL.replace(/\/$/, "");
+
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
